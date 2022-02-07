421b557f03cb6a8afdd876d1b69a2e8e944bfda7
k3nsah <kelcimensah@outlook.com>
2022-02-07 14:51:05 -0500

    👩🏾‍💻 added clickable information display into the terminal

--- pa1/matplotlibxpyplot.py
+++ pa1/matplotlibxpyplot.py
@@ -54,9 +54,11 @@ with open(sys.argv[1]) as f:
 # -------------------------------------------------------------------------- #
 #   H - Matrix Window Properties                            
 # -------------------------------------------------------------------------- #
-fig, ax = plt.subplots(figsize=(7,5), num="CS440 - Intro to Artificial Intelligence")
+fig, ax = plt.subplots(figsize=(8,5), num="CS440 - Intro to Artificial Intelligence")
 
-plt.title('Problem 1: Any-Angle Path Planning')
+
+plt.title("Problem 1: Any-Angle Path Planning")
+# plt.title('Click on plotted points for the g(), f(), and h() values!', fontsize=12) 
 
 plt.gca().invert_yaxis()
 plt.gca().xaxis.tick_top()
@@ -73,13 +75,37 @@ for j in range(gridsize[1]+1):
 plt.xticks(np.arange(gridsize[0]+1), xaxis)
 plt.yticks(np.arange(gridsize[1]+1), yaxis)
 
-cmap = colors.ListedColormap(['white','dimgrey'])
-plt.pcolor(blockgrid[::],cmap=cmap, edgecolors='k', linewidths=1)
+cmap = colors.ListedColormap(['white','steelblue']) # https://matplotlib.org/2.0.2/examples/color/named_colors.html
+plt.pcolor(blockgrid[::],cmap=cmap, edgecolors='k', linewidths=0.5)
 
 # -------------------------------------------------------------------------- #
-#   H - Mapping Plot Points                              
+#   H - Mapping Plot Points (w/ Interactive Information)                             
 # -------------------------------------------------------------------------- #
-ax.plot(startpoint[0]-1, startpoint[1]-1, gid=1, marker='o')
-ax.plot(goalpoint[0]-1, goalpoint[1]-1, gid=2, marker='o')
+# markingsize = 40
+
+def on_pick(event):
+    artist = event.artist
+    xmouse, ymouse = event.mouseevent.xdata, event.mouseevent.ydata
+    x, y = artist.get_xdata(), artist.get_ydata()
+    ind = event.ind
+    # print ('Artist picked:', event.artist)
+    # print ('{} Vertices Picked'.format(len(ind)))
+    # print ('Pick between vertices {} and {}'.format(min(ind), max(ind)+1))
+    # print ('x, y of mouse: {:.2f},{:.2f}'.format(xmouse, ymouse))
+    print ('Path Point:', x[ind[0]]+1, y[ind[0]]+1)
+
+tolerance = 70 # number of points plotted
+
+x_values = [startpoint[0]-1, goalpoint[0]-1]
+y_values = [startpoint[1]-1, goalpoint[1]-1]
+# plt.scatter(x_values, y_values, s=markingsize) 
+plt.plot(x_values, y_values)
+
+
+
+ax.plot(startpoint[0]-1, startpoint[1]-1, marker='o', picker=tolerance)
+ax.plot(goalpoint[0]-1, goalpoint[1]-1, marker='o', picker=tolerance)
+
+fig.canvas.callbacks.connect('pick_event', on_pick)
 
 plt.show()
